b0590533637ef9bbe3994fa98f6a496ee1c8e5b5
abhi1506manu <abhi2019manu@gmail.com>
2023-09-17 13:49:16 +0530

    register logic

--- server/conntrollers/auth.js
+++ server/conntrollers/auth.js
@@ -3,4 +3,40 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 const {Jwt} = jwt;
 
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+    // console.log(req.body);
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password:passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile:Math.floor(Math.random()*1000),
+      impressions:Math.floor(Math.random()*1000),
+    });
+
+    // console.log(newUser);
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({error:error.message});
+  }
+};
