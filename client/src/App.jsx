1393b3996db40a131b39a4f2466218095255073f
abhi1506manu <abhi2019manu@gmail.com>
2023-09-19 12:20:31 +0530

    added theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,31 @@
-import { BrowserRouter, Navigate,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
-import Home from "scenes/homepage/Home"
-import Login from "scenes/loginPage/Login"
-import Profile from "scenes/profilePage/Profile"
-
-
-
-
+import { Home,Login,Profile } from "./scenes";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "./theme.js";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+
   return (
-   <div className='app'>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/profile/:userId" element={<Profile/>}/>
-    </Routes>
-    </BrowserRouter>
-   </div>
-  )
+    <div className="app">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/scenes/index.js
@@ -0,0 +1,7 @@
+import Home from "./homepage/Home";
+import Login from "./loginPage/Login";
+import Navbar from "./navbar/Navbar";
+import Profile from "./profilePage/Profile";
+import Widgets from "./widgets/Widgets";
+
+export {Home, Login,Navbar,Profile,Widgets};
