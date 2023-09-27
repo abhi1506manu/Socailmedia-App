c51f6057591ef569e47225fb86b7e679985860b4
abhi1506manu <abhi2019manu@gmail.com>
2023-09-27 19:02:28 +0530

    completed user profile ui

--- client/src/scenes/index.js
+++ client/src/scenes/index.js
@@ -2,7 +2,7 @@ import Home from "./homepage/Home";
 import Login from "./loginPage/Login";
 import Navbar from "./navbar/Navbar";
 import Profile from "./profilePage/Profile";
-import Widgets from "./widgets/Widgets";
 import Form from "./loginPage/Form"
+import UserWidget from "./widgets/UserWidget";
 
-export {Home, Login,Navbar,Profile,Widgets};
+export {Home, Login,Navbar,Profile,UserWidget};
