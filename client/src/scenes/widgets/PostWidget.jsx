4584c653dbd9ee57af9f782b1ba2fe186f4a649a
abhi1506manu <abhi2019manu@gmail.com>
2023-10-05 07:31:58 +0530

    add or remove friends logic

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
 
-const PostWidget = () => {
-  return (
-    <div>PostWidget</div>
-  )
-}
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Friends from "../../components/Friends";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "../../state";
 
-export default PostWidget
+const PostWidget = ({
+  key,
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+  const [isComments, setIsComments] = useState(false)
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state)=>state.user._id);
+  const isLiked  = Boolean(likes[loggedInUserId]);
+
+  const { palette } = useTheme();
+  const primmaryLight = palette.primary.light;
+  const primmaryDark = palette.primary.dark;
+  const main = palette.primary.main;
+  const medium = palette.primary.medium;
+
+  return <div>PostWidget</div>;
+};
+
+export default PostWidget;
